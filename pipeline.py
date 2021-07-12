ee35f0f811044401c663878eb4373b0a4830e6a7
AviadSar <aviads2@mail.tau.ac.il>
2021-07-12 11:55:48 +0300

    -misc

--- pipeline.py
+++ pipeline.py
@@ -27,14 +27,9 @@ if __name__ == '__main__':
     args = parse_args()
 
     # if args.data_loader_args:
-    #     os.system("data_loader.py --json_file " + args.data_loader_args)
+    #     os.system("python3 data_loader.py --json_file " + args.data_loader_args)
     # if args.trainer_args:
     #     os.system("python3 trainer.py --json_file " + args.trainer_args)
 
-    os.system("python3 trainer.py --json_file " + "linux_args/trainers/roberta-base/missing_middle_5_sentences_out_of_11/text_target/100k/011/trainer_args.json")
-    os.system("python3 trainer.py --json_file " + "linux_args/trainers/roberta-base/missing_middle_5_sentences_out_of_11/text_target/100k/0125/trainer_args.json")
-    os.system("python3 trainer.py --json_file " + "linux_args/trainers/roberta-base/missing_middle_5_sentences_out_of_11/text_target/100k/015/trainer_args.json")
-    os.system("python3 trainer.py --json_file " + "linux_args/trainers/roberta-base/missing_middle_5_sentences_out_of_11/text_target/10k/01/trainer_args.json")
-    os.system("python3 trainer.py --json_file " + "linux_args/trainers/roberta-base/missing_middle_5_sentences_out_of_11/text_target/10k/011/trainer_args.json")
-    os.system("python3 trainer.py --json_file " + "linux_args/trainers/roberta-base/missing_middle_5_sentences_out_of_11/text_target/10k/0125/trainer_args.json")
-    os.system("python3 trainer.py --json_file " + "linux_args/trainers/roberta-base/missing_middle_5_sentences_out_of_11/text_target/10k/015/trainer_args.json")
+    os.system("python3 data_loader.py --json_file " + "linux_args/data_loaders/missing_middle_5_sentences_out_of_11/text_target_with_clue/loader_args.json")
+    os.system("python3 trainer.py --json_file " + "linux_args/trainers/roberta-base/missing_middle_5_sentences_out_of_11/text_target_with_clue/1m/01/trainer_args.json")
